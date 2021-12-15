b7445a8ae253f91087c542cb15b36bed5a9ee206
KadenIB <24burciaga.kaden@waipahuh.k12.hi.us>
2021-12-15 12:13:32 -1000

    First Commit

---
+++ Java.js
@@ -0,0 +1,35 @@
+function one() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function two() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function three() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function four() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function five() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function six() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function seven() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function eight() {
+  setTimeout(function(){ alert("Yay, You did it!"); }, 100);
+  }
+
+function nine() {
+  setTimeout(function(){ alert("Try Again"); }, 100);
+  }
